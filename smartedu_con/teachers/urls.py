fe731307a99f033abf3d6107be101a1a1f48b683
İsmail Çınar <ismailcinar99@gmail.com>
2022-03-08 22:57:51 +0300

    DetailView

--- smartedu_con/teachers/urls.py
+++ smartedu_con/teachers/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from teachers.views import TeacherListView
+from teachers.views import TeacherListView, TeacherDetailView
 
 urlpatterns = [
     path('', TeacherListView.as_view(), name="teachers"),
+    path('teacher/<int:pk>', TeacherDetailView.as_view(), name="teacher_detail"),
+    
    
    
 ]
--- smartedu_con/teachers/views.py
+++ smartedu_con/teachers/views.py
@@ -1,8 +1,10 @@
+from datetime import timezone
 from multiprocessing import context
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from teachers.models import Teacher
-
+from courses.models import Course
 class TeacherListView(ListView):
     model = Teacher
     template_name = 'teachers.html'
@@ -12,4 +14,13 @@ class TeacherListView(ListView):
     def get_queryset(self):
         return  Teacher.objects.all()[:3] # içeride kaç tane göstermek istediğmizi seçiyoruz
     
-    # paginate_by kullanarak da bunu sağlayabiliriz html koduna hazır kod yerleştirerek
+    # paginate_by kullanarak da bunu sağlayabiliriz html koduna hazır kod yerleştirerek,
+class TeacherDetailView(DetailView):
+    model = Teacher
+    template_name = 'teacher.html'
+    context_object_name = 'teacher'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['courses'] = Course.objects.filter(available=True, teacher = self.kwargs['pk'])
+        return context
