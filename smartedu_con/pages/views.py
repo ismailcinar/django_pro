997e0271f1eebef792cfb872687ce153b589982f
İsmail Çınar <ismailcinar99@gmail.com>
2022-03-11 22:43:32 +0300

    cSRF

--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -2,8 +2,15 @@ from re import template
 from turtle import Turtle
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from courses.models import Course
+from django.views.generic.edit import FormView
+from . forms import ContactForm
+from django.contrib.messages.views import SuccessMessageMixin 
+
+
+
 #def index(request):
 #    return render(request,'index.html')
 class IndexView(TemplateView):
@@ -18,3 +25,14 @@ class AboutView(TemplateView):
     template_name = 'about.html'
 #def about(request):
 #    return render(request,'about.html')
+
+class ContactView(SuccessMessageMixin, FormView):
+    template_name = 'contact.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('contact')
+    success_message = 'We received your request'
+    #reverselazy ilgili urlye döner
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
