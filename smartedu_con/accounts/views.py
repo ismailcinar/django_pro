b34150e52d82625ae3d5df5dfff67d5b4bf8410e
İsmail Çınar <ismailcinar99@gmail.com>
2022-03-13 22:33:52 +0300

    login

--- smartedu_con/accounts/views.py
+++ smartedu_con/accounts/views.py
@@ -1,10 +1,37 @@
 
 
 # Create your views here.
-from django.shortcuts import get_object_or_404, render
+from pickle import READONLY_BUFFER
+from django.shortcuts import get_object_or_404, redirect, render
+from . forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def user_login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username,
+                                         password=password)
+            
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('index')
+                
+                else:
+                    messages.info(request, 'Disabled Account')
+            
+            else:
+                messages.info(request, 'Check Your Usernama and password')
+
+    else:
+        form = LoginForm
+    
+    return render(request, 'login.html', {'form': form})
+
 
 def user_register(request):
     return render(request, 'register.html')
