c204835102cc3c7106a57431adffc775b18ccfe3
İsmail Çınar <ismailcinar99@gmail.com>
2022-03-16 21:54:19 +0300

    done

--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -1,5 +1,8 @@
 from multiprocessing import context
 from django.shortcuts import get_object_or_404, render
+from django.test import tag
+
+from accounts.views import enroll_the_course
 from . models import Category, Course, Tag
 
 from django.http import HttpResponse
@@ -21,6 +24,7 @@ def course_list(request, category_slug=None, tag_slug=None):
     tag_page = None
     categories = Category.objects.all()
     tags = Tag.objects.all()
+    current_user = request.user
 
     if category_slug != None:
         category_page = get_object_or_404(Category, slug=category_slug)
@@ -31,8 +35,14 @@ def course_list(request, category_slug=None, tag_slug=None):
         courses = Course.objects.filter(available=True, tags=tag_page)
 
     else:
-        courses = Course.objects.all().order_by('-date')
-        
+       # courses = Course.objects.all().order_by('-date')
+       if current_user.is_authenticated:
+           enrolled_courses = current_user.courses_joined.all()
+           courses = Course.objects.all().order_by('-date')
+           for course in enrolled_courses:
+               courses = courses.exclude(id = course.id)
+       else:
+             courses = Course.objects.all().order_by('-date')
     context = {
         'courses': courses,
         'categories': categories,
@@ -44,9 +54,20 @@ def course_list(request, category_slug=None, tag_slug=None):
 
 def course_detail(request, category_slug, course_id):
     course = Course.objects.get(category__slug = category_slug, id = course_id)
-    
+    current_user = request.user
+    courses = Course.objects.all().order_by('-date')
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+    if current_user.is_authenticated:
+        enrolled_courses = current_user.courses_joined.all()
+
+    else:
+        enrolled_courses = courses
     context = {
-           'course' : course
+           'course' : course,
+           'enrolled_courses' : enrolled_courses,
+           'categories' : categories,
+           'tags' : tags
     }
     return render(request,'course.html', context)
 
