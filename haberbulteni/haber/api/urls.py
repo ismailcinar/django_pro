b0f593189436cf225a8487a30238697448a578d3
İsmail Çınar <ismailcinar99@gmail.com>
2022-03-25 22:40:21 +0300

    apiview

--- haberbulteni/haber/api/urls.py
+++ haberbulteni/haber/api/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from haber.api import views as api_views
 
 urlpatterns = [
-    path('makaleler', api_views.makale_list_create_api_view, name="makale-listesi"),
+    path('makaleler/',api_views.MakaleListCreateAPIView.as_view(), name='makale-listesi'),
+    path('makaleler/<int:pk>', api_views.MakaleDetailAPIView.as_view(), name='makale-detay'),
+
 ]
+
+
+
+# urlpatterns = [  FUNVTUINO BASED VIEWS
+#     path('makaleler/', api_views.makale_list_create_api_view, name="makale-listesi"),
+#     path('makaleler/<int:pk>', api_views.makale_detail_api_view, name="makale-detay"),
+
+# ]
