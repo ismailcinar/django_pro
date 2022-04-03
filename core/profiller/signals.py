4fc8681046ec841568a8b66827f40e0eb79aec62
İsmail Çınar <ismailcinar99@gmail.com>
2022-04-04 00:46:04 +0300

    signals

---
+++ core/profiller/signals.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from profiller.models import Profil
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_profil(sender, instance, created, **kwargs):
+    print(instance.username, '__Created', created)
+    if created:
+        Profil.objects.create(user=instance)
+         # her defasında farklı bir nesne için profil oluşturuyoruz
+        
