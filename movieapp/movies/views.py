47e128bf9981acbf55ba1c0c64138e630edd87e2
İsmail Çınar <ismailcinar99@gmail.com>
2022-07-06 00:59:23 +0300

    templates

--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def movies(request):
+    return render(request, 'movies.html')
+
+
+def movie_details(request, slug):
+    return render(request, 'movie-details.html', {
+        "slug":slug
+    })
+
+# return render("details:"+ slug)
