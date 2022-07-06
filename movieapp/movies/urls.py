9e523b979a2d8a369f3a48a14898bccee9b58510
İsmail Çınar <ismailcinar99@gmail.com>
2022-07-07 02:48:30 +0300

    partial

--- movieapp/movies/urls.py
+++ movieapp/movies/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.index),
-    path("movies",views.movies),
-    path("movies/<slug:slug>",views.movie_details)
+    path("",views.index, name="home_page"),
+    path("movies",views.movies, name="movies_page"),
+    path("movies/<slug:slug>",views.movie_details, name="movies_details")
 ]
--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -1,16 +1,86 @@
+from datetime import date
 from django.http import HttpResponse
 from django.shortcuts import render
 
+data = {
+    "movies": [
+        {
+          "title": "film 1 adı",
+          "description": "açıklama",
+          "imageUrl": "m1.jpg",
+          "coverImage": "cover1.jpg",
+          "slug":"film-adi-1",
+          "language": "english",
+          "date": date(2022,1,2)
+        },
+         {
+          "title": "film 2 adı",
+          "description": "açıklama",
+          "imageUrl": "m2.jpg",
+          "coverImage": "cover2.jpg",
+          "slug":"film-adi-2",
+          "language": "english",
+          "date": date(2022,1,3)
+        },
+         {
+          "title": "film 3 adı",
+          "description": "açıklama",
+          "imageUrl": "m3.jpg",
+          "coverImage": "cover3.jpg",
+          "slug":"film-adi-3",
+          "language": "english",
+          "date": date(2022,1,4)
+        },
+         {
+          "title": "film 4 adı",
+          "description": "açıklama",
+          "imageUrl": "m4.jpg",
+          "slug":"film-adi-4",
+          "language": "english",
+          "date": date(2022,4,2)
+        }
+    ],
+    "sliders": [
+        {
+        "slider_image":"slider1.jpg",
+        "url":"film-adi-1"
+        },
+         {
+        "slider_image":"slider2.jpg",
+        "url":"film-adi-2"
+        },
+         {
+        "slider_image":"slider3.jpg",
+        "url":"film-adi-3"
+        }
+    ]
+}
+
 def index(request):
-    return render(request, 'index.html')
+    movies = data["movies"]
+    sliders = data["sliders"]
+    return render(request, 'index.html', {
+        "movies": movies,
+        "sliders":sliders
+
+    }) 
 
 def movies(request):
-    return render(request, 'movies.html')
+    movies = data["movies"]
+    return render(request, 'movies.html', {
+        "movies": movies
+    })
 
 
 def movie_details(request, slug):
+    movies = data["movies"]
+    selectedMovie = None
+    for movie in movies:
+        if movie["slug"] == slug:
+            selectedMovie = movie
+
     return render(request, 'movie-details.html', {
-        "slug":slug
+        "movie": selectedMovie
     })
 
 # return render("details:"+ slug)
